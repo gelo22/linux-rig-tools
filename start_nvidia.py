7fef5647a7fccd3ea1fbb7342c1124137da2c751
Anatoliy Kurinnyy <one.quaker@gmail.com>
2017-10-09 16:30:06 +0300

    nvidia_start.py added

---
+++ start_nvidia.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import logging as log
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LOG_LEVEL = log.DEBUG
+log.basicConfig(format='[%(levelname)s] %(message)s', level=LOG_LEVEL)
+
+
+api_fp = os.path.join(ROOT_DIR, 'api', 'api.py')
+oc_fp = os.path.join(ROOT_DIR, 'nvset.py')
+ini_fp = os.path.join(ROOT_DIR, 'oc.ini')
+
+path_list =(
+    api_fp,
+    oc_fp,
+    ini_fp
+)
+
+
+def check_fp(l):
+    for i in l:
+        if os.path.exists(i):
+            log.info('{} [exists]'.format(i))
+        else:
+            log.error('{} [not found]'.format(i))
+            sys.exit(1)
+
+check_fp(path_list)
+
+
+os.system('tmux new -d -s api \"python3 {} --api --gpu-type nvidia --getdata-interval 10\"'.format(api_fp))
+os.system('tmux new -d -s oc  \"python3 {} -c {} -D\"'.format(oc_fp, ini_fp))
--- start_nvidia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ROOT_DIR=$1
-
-cd ROOT_DIR
-
-tmux new -d -s api "python3 api/api.py --api --gpu-type nvidia --getdata-interval 10"
-tmux new -d -s oc  "python3 nvset.py -c oc.ini -D"

938cf2defbb4b15d1f5ea0712d956609d2199208
Anatoliy Kurinnyy <one.quaker@gmail.com>
2017-10-09 16:10:32 +0300

    start_nvidia fix

--- start_nvidia.sh
+++ start_nvidia.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 ROOT_DIR=$1
-tmux new -d -s api "python3 $ROOT_DIR/api/api.py --api --gpu-type nvidia --getdata-interval 10"
-tmux new -d -s oc  "python3 $ROOT_DIR/nvset.py -c oc.ini -D"
+
+cd ROOT_DIR
+
+tmux new -d -s api "python3 api/api.py --api --gpu-type nvidia --getdata-interval 10"
+tmux new -d -s oc  "python3 nvset.py -c oc.ini -D"
